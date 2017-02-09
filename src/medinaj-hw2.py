bf6ce197c76065698abc3fa1363ea5bd6f37007c
Jeremy Medina <Jeremy Medina>
2017-02-09 13:18:10 -0700

    'Finished'

---
+++ src/medinaj-hw2.py
@@ -0,0 +1,6 @@
+# Jeremy Medina
+# 2-9-17 HW 2
+import numpy
+num_list = numpy.array([0.1, 0.8, 1.6, 2.1, 2.9, 9.7])
+print 'This is a simple program using numpy to round decimals to the nearest whole integer.\nThe original list is:',num_list
+print 'The rounded list is:',numpy.rint(num_list)
